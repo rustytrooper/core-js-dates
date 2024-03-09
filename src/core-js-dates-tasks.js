c7065a3b7285697611cef22597cbeb1e6052a873
rustytrooper <diansolo40@gmail.com>
2024-03-10 01:31:46 +0300

    init: done 5 tests

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,10 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  const currentDate = Date.parse(date);
+  const startDate = Date.parse('01 Jan 1970 00:00:00 GMT');
+  return currentDate - startDate;
 }
 
 /**
@@ -31,8 +33,11 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  return `${hours < 10 ? '0'.concat(String(hours)) : hours}:${minutes < 10 ? '0'.concat(String(minutes)) : minutes}:${seconds < 10 ? '0'.concat(String(seconds)) : seconds}`;
 }
 
 /**
@@ -46,8 +51,18 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const weekDay = new Date(date).getDay();
+  return weekDays[weekDay];
 }
 
 /**
@@ -76,8 +91,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month, 0).getDate();
 }
 
 /**
@@ -91,8 +106,12 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const dayStart = new Date(dateStart);
+  const dayEnd = new Date(dateEnd);
+  const mlsecDiffer = Math.abs(dayEnd.getTime() - dayStart.getTime());
+  const mlsecPerDay = 24 * 60 * 60 * 1000;
+  return Math.round(mlsecDiffer / mlsecPerDay) + 1;
 }
 
 /**
